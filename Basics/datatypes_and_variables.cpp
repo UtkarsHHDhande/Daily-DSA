161a47dea35668b53328ce5eea44ef483bf339f1
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-07-08 01:35:35 +0530

    datatypes and variables

---
+++ Basics/datatypes_and_variables.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+int main(){
+   // int a = 369; 
+   // char b = 'v';
+   // bool c = true;
+   // float d = 369.369;
+   // double e = 108.108;
+
+   // DATA TYPES
+
+   // cout<<"The value of a is: "<<a<<endl;
+   // cout<<"The value of b is: "<<b<<endl;
+   // cout<<"The value of c is: "<<c<<endl;
+   // cout<<"The value of d is: "<<d<<endl;
+   // cout<<"The value of e is: "<<e<<endl;
+
+   // SIZE OF DATA TYPES
+
+   // int size = sizeof(a);
+   // cout<<size<<endl;
+
+// Type Casting
+//   int a = 'a';
+//   cout<<a<<endl;
+
+//   char ch = 98;
+//   cout<<ch<<endl;
+
+// unsigned int a = -112;
+// cout<<a<<endl;
+
+// Operators in C++
+
+// Arithmetic Operators
+// + , - , * , / , % , ++ , --
+
+//  float a = 2.0/5;
+//  cout<<a<<endl;
+
+// Relational Operators
+// == , != , > , < , >= , <=
+
+//  int a = 5, b = 6;
+
+// bool first = a==b;
+// cout<<first<<endl;
+
+// bool second = a>b;
+// cout<<second<<endl;
+
+// bool third = a<b;
+// cout<<third<<endl;
+
+
+// Assignment Operators
+// = , += , -= , *= , /= , %=
+
+
+// Logical Operators
+// && , || , !
+
+
+   return 0;
+ 
+}
