2924bab42507c0227e7ccb5a8e6b296cc2f6aa51
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-07-08 22:43:29 +0530

    loops

---
+++ Basics/loop.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int a;
+    // cin>>a;
+
+    // if (a>0)
+    // {
+    //     cout << "The number is positive" << endl;
+    // }
+    // else
+    // {
+    //     cout << "The number is negative" << endl;
+    // }
+
+    // int a,b;
+
+    // cout<<"Enter the value of a: "<<endl;
+    // cin>>a;
+    // cout<<"Enter the value of b: "<<endl;
+    // cin>>b;
+
+    // if (a>b)
+    // {
+    //     cout<<"a is greater than b"<<endl;
+    // }
+    // else
+    // {
+    //     cout<<"b is greater than a"<<endl;
+    // }
+   
+//  int num;
+//  cout<<"Enter the number: "<<endl;
+//  cin>>num;
+
+// if ( num > 0)
+// {
+//   cout<<"The number is positive"<<endl;
+// }
+// else if (num < 0)
+// {
+//     cout<<"The number is negative"<<endl;
+// }
+// else
+// {
+//     cout<<"The number is zero"<<endl;
+// }
+
+// HOMEWORK QUESTION =>
+
+// char a;
+// cin>>a;
+
+// if ( a >= 'a' && a <= 'z')
+// {
+//     cout<<"The character is lowercase"<<endl;
+// }
+// else if (a >= 'A' && a <= 'Z')
+// {
+//     cout<<"The character is uppercase"<<endl;
+// }
+// else if(a >= '0' && a <= '9')
+// {
+//     cout<<"The character is a digit"<<endl;
+// }
+// else
+// {
+//     cout<<"The character is not an alphabet"<<endl;
+// }
+
+
+// WHILE LOOP
+
+// int n;
+// cin>>n;
+
+// int i = 1;
+// while(i <= n){
+//     cout<<i<<endl;
+//     i = i + 1;
+// }
+
+
+// Addition of first n natural numbers
+// int sum = 0;
+// int i = 0;
+// int n;
+// cin>>n;
+// while (i<=n)
+// {
+//     sum = sum + i;
+//     i = i + 1;
+// }
+// cout<<sum<<endl;
+
+
+// Addition of first n even numbers
+
+// int sum = 0;
+// int i = 0;
+// int n;
+// cin>>n;
+// while (i<=n)
+// {
+//     sum = sum + i;
+//     i = i + 2;
+// }
+// cout<<sum<<endl;
+
+// Prime or not
+
+// int n;
+// cin>>n;
+
+// int i = 2;
+// while (i<n)
+// {
+//    if (n%i == 0)
+//    {
+//    cout<<"Not a Prime"<<endl;
+//    }
+  
+//    else
+//    {
+//     cout<<"Prime"<<i<<endl;
+//    }
+   
+//     i = i+1;
+// }
+
+
+
+
+ return 0;
+}
