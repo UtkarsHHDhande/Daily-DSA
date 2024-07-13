6ad0c00bd8d7947717fa2ebe1a09ee701d983ab9
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-07-13 23:09:38 +0530

    pattern 11

---
+++ Patterns/pattern11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    int i;
+    cin>>n;
+    for ( i = 0; i < n ; i++)
+    {
+        int j;
+       for ( j = 0; j < n; j++)
+       {
+        cout<<"* ";
+       }
+
+       cout<<endl;
+    }
+    
+}
