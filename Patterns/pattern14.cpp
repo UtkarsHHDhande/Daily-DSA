17326b54fc8ed90e2c9502f9bb2ff9ce1b2b25e5
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-07-13 23:25:04 +0530

    pattern 14

---
+++ Patterns/pattern14.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i;
+    for ( i = 1; i <= n; i++)
+    {
+        int j;
+       for ( j = 1; j <= i; j++)
+       {
+        cout<<i;
+       }
+       cout<<endl;
+        
+    }
+    
+}
