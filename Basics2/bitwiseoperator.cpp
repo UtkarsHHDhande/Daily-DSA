ce8ef3cbe4149c28e8e9806ab499defd8707cdfe
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-07-21 23:22:57 +0530

    bit wise operator

---
+++ Basics2/bitwiseoperator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 4;
+    int b = 6;
+     
+    //  cout<<(a & b)<<endl; // 4
+    //  cout<<(a | b)<<endl; // 6
+    //  cout<<(a ^ b)<<endl; // 2
+    //  cout<<(~a)<<endl; // -5
+
+
+// LEFT SHIFT & RIGHT SHIFT
+
+    //  cout<<(17>>1)<<endl; 
+    //  cout<<(17>>2)<<endl;
+    //  cout<<(19<<1)<<endl;
+    //  cout<<(21<<2)<<endl;
+
+    // PRE & POST INCREMENT/DECREMENT
+
+  int i = 7;
+  cout<<++i<<endl; //8
+  cout<<i++<<endl; //8
+  cout<<i--<<endl; // 9
+  cout<<--i<<endl;// 7
+    
+}
