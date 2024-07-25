620f6d3f461c0969436252a03cb842ec80946ab3
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-07-25 08:05:14 +0530

    functions

---
+++ Basics2/functions.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+using namespace std;
+
+// int power (int a,int b){
+//     int ans = 1;
+//     for (int i = 1; i <= b; i++)
+//     {
+//        ans = ans * a;
+//     }
+//     return ans;
+    
+// }
+// int main(){
+// int a, b;
+// cin>> a>> b;
+
+// int ans = power(a,b);
+// cout<<ans;
+
+// }
+
+// bool isEven(int a){
+//     if(a&1){
+//         return 0;
+//     }
+//     else{
+//          return 1;
+//     }
+   
+// }
+
+// int main(){
+//     int num;
+//     cin>> num;
+
+// if (isEven(num)){
+//     cout<<"Number is Even"<<endl;
+
+// }
+// else{
+//     cout<<"Number is Odd"<<endl;
+// }
+
+//     return 0;
+
+// }
+
+// int fact(int n){
+//     int fact = 1;
+
+// for (int i = 1; i <= n; i++)
+// {
+//     fact = fact * i;
+// }
+// return fact;
+// }
+
+
+
+// int nCr(int n, int r){
+
+// int num = fact(n);
+
+// int deno = fact(r) * fact (n-r);
+
+//  return  num/deno;
+
+
+// }
+
+// int main(){
+//   int n,r;
+//     cin>>n >>r;
+
+//  cout<<"Answer is "<< nCr(n,r)<<endl;
+
+//     return 0;
+// }
+
+// void printCounting(int n){
+
+//  for(int i = 1 ; i<= n;i++){
+//         cout<< i << " ";
+//     }
+// }
+
+
+// int main(){
+
+// int n;
+// cin>> n;
+
+// printCounting(n);
+
+
+// return 0;
+// }
+
+
+// bool isPrime(int n){
+//     if(n<=1){
+//         return false;
+//     }
+//     for (int i = 2; i< n; i++)
+//     {
+//         if(n%i==0){
+//             return false;
+//         }
+//     }
+//     return true;
+    
+// }
+// int main(){
+//     int n;
+//     cin>> n;
+
+//     if(isPrime(n)){
+//         cout<<"Prime Number"<<endl;
+//     }
+//     else{
+//         cout<<"Not a Prime Number"<<endl;
+//     }
+// }
